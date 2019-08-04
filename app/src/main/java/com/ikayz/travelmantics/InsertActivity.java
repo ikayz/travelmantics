5c494e8607a893c11cdb7dbf0890e1566001f5a4
Isaac Miti <ikayz@live.com>
2019-08-04 19:11:28 +0200

    Add save menu resource file

--- app/src/main/java/com/ikayz/travelmantics/InsertActivity.java
+++ app/src/main/java/com/ikayz/travelmantics/InsertActivity.java
@@ -3,12 +3,21 @@ package com.ikayz.travelmantics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class InsertActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_insert);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.save_menu, menu);
+        return true;
     }
 }
