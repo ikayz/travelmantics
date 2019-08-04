f0f1996e3eddec61053dffe24cdd89da229a2bf2
Isaac Miti <ikayz@live.com>
2019-08-04 19:29:17 +0200

    Add contructors or travel deals

---
+++ app/src/main/java/com/ikayz/travelmantics/TravelDeals.java
@@ -0,0 +1,20 @@
+package com.ikayz.travelmantics;
+
+/* 
+Created by ikayz on 04/08/2019
+*/
+public class TravelDeals {
+    private String id;
+    private String title;
+    private String description;
+    private String price;
+    private String imageUrl;
+
+    public TravelDeals(String id, String title, String description, String price, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+}
