d77e3e6d979e9ed781a2aaf81fb9d4ce3117b34c
Isaac Miti <ikayz@live.com>
2019-08-05 00:24:24 +0200

    Add logging code to adapter

--- app/src/main/java/com/ikayz/travelmantics/DealAdapter.java
+++ app/src/main/java/com/ikayz/travelmantics/DealAdapter.java
@@ -1,15 +1,82 @@
 package com.ikayz.travelmantics;
 
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 /*
 Created by ikayz on 04/08/2019
 */
-public class DealAdapter {
+public class DealAdapter extends RecyclerView.Adapter<DealAdapter.DealViewHolder>{
+//    @NonNull
+    ArrayList<TravelDeal> deals;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mDatabaseReference;
+    private ChildEventListener mChildEventListener;
+
+    public DealAdapter() {
+        FirebaseUtil.openFbReference("traveldeals");
+        mFirebaseDatabase = FirebaseUtil.mFirebaseDatabase;
+        mDatabaseReference = FirebaseUtil.mDatabaseReference;
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                TravelDeal td = dataSnapshot.getValue(TravelDeal.class);
+                Log.d("Deal: ", td.getTitle());
+                td.setId(dataSnapshot.getKey());
+                deals.add(td);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+    }
+
+    @Override
+    public DealViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DealViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class DealViewHolder extends RecyclerView.ViewHolder {
         TextView tv_title;
         public DealViewHolder(@NonNull View itemView) {
