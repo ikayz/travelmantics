c1c74e617e90e362c9571acb36791c20a250b26e
Isaac Miti <ikayz@live.com>
2019-08-05 10:51:29 +0200

    Create sign in method

--- app/src/main/java/com/ikayz/travelmantics/FirebaseUtil.java
+++ app/src/main/java/com/ikayz/travelmantics/FirebaseUtil.java
@@ -1,12 +1,17 @@
 package com.ikayz.travelmantics;
 
+import android.app.Activity;
+
 import androidx.annotation.NonNull;
 
+import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /*
 Created by ikayz on 04/08/2019
@@ -18,14 +23,17 @@ public class FirebaseUtil {
     public static FirebaseAuth mFirebaseAuth;
     public static FirebaseAuth.AuthStateListener mAuthListener;
     public  static ArrayList<TravelDeal> mDeals;
+    private static final int RC_SIGN_IN = 123;
+    private static Activity caller;
 
     private FirebaseUtil() {};
 
-    public static void openFbReference(String ref) {
+    public static void openFbReference(String ref, final Activity callerActivity) {
         if(sFirebaseUtil == null) {
             sFirebaseUtil = new FirebaseUtil();
             mFirebaseDatabase = FirebaseDatabase.getInstance();
             mFirebaseAuth = FirebaseAuth.getInstance();
+            caller = callerActivity;
             mAuthListener = new FirebaseAuth.AuthStateListener() {
                 @Override
                 public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -35,6 +43,21 @@ public class FirebaseUtil {
         }
         mDeals = new ArrayList<TravelDeal>();
         mDatabaseReference = mFirebaseDatabase.getReference().child(ref);
+    }
+
+    private static void signIn() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create and launch sign-in intent
+        caller.startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
     }
 
     public static void attachListener() {
